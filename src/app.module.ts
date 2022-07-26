47e1007eba0499d7d73ec0970797a9ffb5d1ce41
Roberto Minelli <roberto.minelli@ust.com>
2022-07-26 15:44:36 +0200

    FIX: e2e test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import configuration from 'config/configuration';
+import configuration from '../config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GridModule } from './grid/grid.module';
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,21 +2,40 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule,
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: (configService: ConfigService) => ({
+            type: 'postgres',
+            host: configService.get('DATABASE_HOST'),
+            port: +configService.get('DATABASE_PORT'),
+            username: configService.get('POSTGRES_USER'),
+            password: configService.get('POSTGRES_PASSWORD'),
+            database: configService.get('POSTGRES_DB'),
+            entities: ['./dist/src/**/**/*.js'],
+            synchronize: true,
+            logging: true,
+          }),
+          inject: [ConfigService],
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+  it('/ (GET)', async () => {
+    return await request(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Airbus Aerospace Systems - Sun Spot Analyser Test');
